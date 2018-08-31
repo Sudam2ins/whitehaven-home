d4958ecddacf70b4be4bb446c384e66053b3df40
Sudam Regmi <sudam2ins@gmail.com>
2018-08-31 19:53:09 +0545

    White Heaven coal home page responsive v1

--- js/custom.js
+++ js/custom.js
@@ -26,13 +26,43 @@ $(function(){
 	$('.s1-tabs ul li a').click(function(e){
 		e.preventDefault();
 		findTarget = $(this).attr('data-target');
-		ifAlreadyActive = $('.ss-items.'+findTarget).hasClass('active');
+		ifAlreadyActive = $('.section-1 .ss-items.'+findTarget).hasClass('active');
 
 		if(!ifAlreadyActive){
-			$('.ss-items').stop().fadeOut(0).removeClass('active');		
-			$('.ss-items.'+findTarget).stop().fadeIn(300).addClass('active');
+			$('.section-1 .ss-items').stop().fadeOut(0);
+			$('.section-1 .ss-items, .s1-tabs ul li ').removeClass('active');
+			$('.section-1  .ss-items.'+findTarget).stop().fadeIn(300);
+			$('.section-1  .ss-items.'+findTarget).addClass('active');
+			$(this).parent().addClass('active');
 		}
-	})
+	});
+
+	$('.s5-tabs .sl-item a').click(function(e){
+		e.preventDefault();
+		findTarget = $(this).attr('data-target');
+		ifAlreadyActive = $('.s5-intro .s5-items.'+findTarget).hasClass('active');
+		console.log(ifAlreadyActive)
+
+		if(!ifAlreadyActive){
+			$('.section-5 .s5-items').stop().fadeOut(0);
+			$('.section-5 .s5-items, .s5-tabs .sl-item a ').removeClass('active');
+			$('.section-5 .s5-items.'+findTarget).stop().fadeIn(750);
+			$('.section-5 .s5-items.'+findTarget).addClass('active');
+			$(this).addClass('active');
+		}
+	});
+
+	// Scroll Reveal
+
+	// Reveal On Scroll Initialization
+	window.sr = ScrollReveal();
+	sr.reveal('.scroll-reveal', {
+		duration: 700,
+		distance: '50px',
+		delay: 0,
+		scale: 1,
+		easing: 'ease-in-out'
+	});
 
 	// Window Resize Orientation Change
 	$(window).bind('resize orientationchange',function(){
