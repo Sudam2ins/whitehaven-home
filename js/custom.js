16d35a098e7ddd6ccfa4fde9598dd7559aeb0880
Sudam Regmi <sudam2ins@gmail.com>
2018-09-01 11:19:55 +0545

    White Haven Coal-Home page Responsive v4

--- js/custom.js
+++ js/custom.js
@@ -37,11 +37,24 @@ $(function(){
 		}
 	});
 
+	$('.s1-tab-selector-mobile select').change(function(e){
+		e.preventDefault();
+		findTarget = $(this).val();
+		ifAlreadyActive = $('.section-1 .ss-item'+findTarget).hasClass('active');
+		console.log(ifAlreadyActive)
+
+		if(!ifAlreadyActive){
+			$('.section-1 .ss-items').stop().fadeOut(0);
+			$('.section-1 .ss-items').removeClass('active');
+			$('.section-1 .ss-item'+findTarget).stop().fadeIn(300);
+			$('.section-1 .ss-item'+findTarget).addClass('active');
+		}
+	});
+
 	$('.s5-tabs .sl-item a').click(function(e){
 		e.preventDefault();
 		findTarget = $(this).attr('data-target');
 		ifAlreadyActive = $('.s5-intro .s5-items.'+findTarget).hasClass('active');
-		console.log(ifAlreadyActive)
 
 		if(!ifAlreadyActive){
 			$('.section-5 .s5-items').stop().fadeOut(0);
@@ -68,4 +81,15 @@ $(function(){
 	$(window).bind('resize orientationchange',function(){
 		wwwh();
 	})
+
+	//Window Scroll
+	$(window).scroll(function(){
+		findScrolledFromTop = $(this).scrollTop();
+		if(findScrolledFromTop > 50){
+			$('.s1-slider .ssi-cta-section').addClass('scrolled');
+		}else{
+			$('.s1-slider .ssi-cta-section').removeClass('scrolled');
+		}
+	})
+
 })
